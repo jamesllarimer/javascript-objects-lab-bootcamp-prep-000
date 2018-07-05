68683ab68f71e982155ad7dd68b063703d99c923
James Lefty Larimer <lefty1044@gmail.com>
2018-07-05 17:35:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var recipes = new Object();
+var recipes = {};
 
-updateObjectWithKeyAndValue(object, key, value){
- object object[key] = value;
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
   return object;
 }
 
-destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 }
