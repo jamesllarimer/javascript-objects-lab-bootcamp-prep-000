955f35af2e8be0be7df1efc4edfecf155e5d965f
James Lefty Larimer <lefty1044@gmail.com>
2018-07-05 17:39:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ var newObj =  Object.assign({}, object);
 newObj[key] = value;
   return newObj;
 }
+function deleteFromObjectByKey(object, key){
+var newObj =  Object.assign({}, object);
+newObj[key]
+  return newObj;
+}
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
