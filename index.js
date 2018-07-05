c37ea7d8c637953bc1641d1b24107dfa0fad0dd7
James Lefty Larimer <lefty1044@gmail.com>
2018-07-05 17:37:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-var newObj =  Object.assign({}, object[key] = value;
-  return object;
+var newObj =  Object.assign({}, object);
+newObj[key] = value;
+  return newObj;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
